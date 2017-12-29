6f01af71f0f684536441c6a8ba43523ae3962a25
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2017-12-29 23:53:45 +0100

    fix examples

--- README.md
+++ README.md
@@ -11,21 +11,22 @@ Latest version: 0.1.0.
 
 ### Set up
 ```java
-Connection cnx = DriverManager.getConnection("/jdbc/url");
-SingleConnectionProvider provider = new SingleConnectionProvider(cnx);
-OracleSql dsl = new OracleSql(provider);
+final Connection cnx = DriverManager.getConnection("/jdbc/url");
+final SingleConnectionProvider provider = new SingleConnectionProvider(cnx);
+final StandardSql sql = new StandardSql(provider);
+
 ```
 
 ### Select data from the database
 ```java
-SqlSelectBuilder builder = dsl.select("ename").from("emp").where("empno").eq().value(1);
-SingleRowExtractor<Integer> extractor = rs -> rs.getString("ename");
-builder.toQuery(extractor).toList();  // returns ["KING"]
+final SqlSelectBuilder builder = sql.select("ename").from("emp").where("empno").eq().value(1);
+final SingleRowExtractor<String> extractor = rs -> rs.getString("ename");
+builder.toQuery(extractor).toList(); // returns ["KING"]
 ```
 
 ## DELETE examples
 ```java
-dsl.deleteFrom("emp")
+sql.deleteFrom("emp")
 .where("job").notEq().value("SALESMAN");
 ````
 Generates the following statement:
@@ -38,7 +39,7 @@ where
 
 ## UPDATE examples
 ```java
-dsl.update("emp").set("salary").raw("salary * 2");
+sql.update("emp").set("salary").raw("salary * 2");
 ```
 Generates the following statement:
 ```SQL
@@ -49,10 +50,10 @@ update emp set
 ## INSERT examples
 ### Simple INSERT
 ```java
-SqlInsertBuilder builder = dsl.insertInto("emp");
-builder.values().set("ename").value("KING");
-builder.values().set("job").value("PRESIDENT");
-builder.values().set("hiredate").value(new java.sql.Date(new Date().getTime()));
+final SqlInsertBuilder builder = sql.insertInto("emp");
+builder.set("ename").value("KING");
+builder.set("job").value("PRESIDENT");
+builder.set("hiredate").value(new java.sql.Date(new Date().getTime()));
 ```
 Generates the following statement:
 ```SQL
@@ -62,7 +63,7 @@ values (?  /* KING */, ?  /* PRESIDENT */, ?  /* 2017-07-22 */)
 
 ### INSERT with subquery
 ```java
-SqlInsertBuilder builder = dsl.insertInto("emp2");
+final SqlInsertBuilder builder = sql.insertInto("emp2");
 builder.subquery("ename", "job").select("ename", "job").from("emp");
 ```
 Generates the following statement:
@@ -75,7 +76,7 @@ from emp
 ## SELECT examples
 ### SELECT with aggregate function
 ```java
-dsl.select("job", "count(*)")
+sql.select("job", "count(*)")
 .from("emp")
 .groupBy("job");
 ```
@@ -88,9 +89,9 @@ group by job
 
 ### Compound statement: UNION
 ```java
-dsl.union(
-    dsl.select("job").from("emp"),
-    dsl.select("job").from("emp2")
+sql.union(
+    sql.select("job").from("emp"),
+    sql.select("job").from("emp2")
 );
 ```
 Generates the following statement:
@@ -104,11 +105,11 @@ from emp2
 
 ### SELECT with AND/OR operators
 ```java
-final SqlSelectBuilder builder = dsl.select("*");
+final SqlSelectBuilder builder = sql.select("*");
 builder.from("emp");
-builder.where(dsl.or(
-    dsl.condition("job").eq().value("SALESMAN"),
-    dsl.condition("name").eq().value("KING")
+builder.where(sql.or(
+    sql.condition("job").eq().value("SALESMAN"),
+    sql.condition("name").eq().value("KING")
 ));
 // Multiple WHERE clauses are joined with AND
 builder.where("hiredate").lt().raw("SYSDATE - 1");
@@ -124,7 +125,7 @@ where
 
 ### SELECT with JOIN clause
 ```Java
-dsl.select("e.ename", "d.deptname")
+sql.select("e.ename", "d.deptname")
 .from("emp e")
 .innerJoin("dept d on e.deptno = d.deptno");
 ```
@@ -137,10 +138,10 @@ inner join dept d on e.deptno = d.deptno
 
 ### Select with subquery
 ```java
-dsl.select("*");
-.from("emp");
+sql.select("*")
+.from("emp")
 .where("deptno").eq().subquery(
-    dsl.select("deptno")
+    sql.select("deptno")
     .from("dept")
     .where("deptname").eq().value("SALES")
 );
@@ -161,10 +162,9 @@ where
 ### Select with IN clause
 ```Java
 final Collection<String> names = Arrays.asList("KING", "ALLEN");
-dsl.select("*")
+sql.select("*")
 .from("emp")
 .where("ename").in_String(names);
-
 ```
 Generates the following statement:
 ```SQL
@@ -175,28 +175,29 @@ where
 ```
 
 ## Batch statement examples
+### Batch statement with input data coming from a Collection
+This is the same example as previously, except the data come from a Collection instead of a Stream.
+```java
+final List<String> values = Arrays.asList("SALES", "ACCOUNTING");
+final List<SqlInsertBuilder> inserts = values.stream().map(ReadmeExamples::createInsertStatement)
+		.collect(Collectors.toList());
+sql.batchStatement(inserts).executeAndCommit();
+```
+
 ### Batch statement with input data coming from a Stream
 In this example, a Stream of strings is converted to a batch sequence of INSERT statements (using the addBatch method in JDBC).
 At no point in time the full data is held in memory.
 ```java
+final Stream<SqlInsertBuilder> stream = Files.lines(Paths.get("c:/my/file.txt"))
+		.map(ReadmeExamples::createInsertStatement);
+// the empty string is used to generate the SQL, but anything else would
+// have been fine since we are dealing with a prepared statement.
+final String sqlString = createInsertStatement("").getSql();
+sql.batchStatement(sqlString, stream).toStatement().executeAndCommit();
+
 private static SqlInsertBuilder createInsertStatement(String deptname) {
-	final SqlInsertBuilder builder = dsl.insertInto("dept");
+	final SqlInsertBuilder builder = sql.insertInto("dept");
 	builder.values().set("deptname").value(deptname);
 	return builder;
 }
-
-public void insertBatch() {
-    Stream<SqlInsertBuilder> stream = Files.lines(Paths.get("c:/my/file.txt")).map(MyClass::createInsertStatement);
-    // the empty string is used to generate the SQL, but anything else would
-    // have been fine since we are dealing with a prepared statement.
-    String sql = createInsertStatement("").getSql();
-    dsl.batchStatement(sql, stream).executeAndCommit();
-}
-```
-### Batch statement with input data coming from a Collection
-This is the same example as previously, except the data come from a Collection instead of a Stream.
-```java
-List<String> values = Arrays.asList("SALES", "ACCOUNTING");
-List<SqlInsertBuilder> inserts = values.stream().map(MyClass::createInsertStatement).collect(Collectors.toList());
-dsl.batchStatement(inserts).executeAndCommit();
 ```
--- src/test/java/com/github/fjdbc/sql/ReadmeExamples.java
+++ src/test/java/com/github/fjdbc/sql/ReadmeExamples.java
@@ -1,80 +1,147 @@
 package com.github.fjdbc.sql;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.github.fjdbc.connection.SingleConnectionProvider;
 import com.github.fjdbc.query.SingleRowExtractor;
+import com.github.fjdbc.sql.StandardSql.SqlInsertBuilder;
 import com.github.fjdbc.sql.StandardSql.SqlSelectBuilder;
-import com.github.fjdbc.sql.StandardSql.SqlUpdateBuilder;
 
 /**
  * The examples used in README.MD
  */
 public class ReadmeExamples {
-	public static void main(String[] args) throws SQLException {
+	private static final StandardSql sql = new StandardSql(null);
+
+	public static void main(String[] args) throws Exception {
 		// -------------------------------------------------------------------------------------------------------------
 		// Setup
-		final Connection connection = null;
-		final StandardSql sql = new StandardSql(new SingleConnectionProvider(connection), true);
+		final Connection cnx = DriverManager.getConnection("/jdbc/url");
+		final SingleConnectionProvider provider = new SingleConnectionProvider(cnx);
+		final StandardSql sql = new StandardSql(provider);
 
-		// Simple SELECT
+		// @formatter:off
+		// Select data from the database
 		{
-			final SqlSelectBuilder builder = sql.select("name");
-			builder.from("user");
-			builder.where("id").eq().value(14);
-			System.out.println(builder.getSql());
-			final SingleRowExtractor<String> extractor = rs -> rs.getString("name");
-			// final List<String> names = builder.toQuery(extractor).toList();
+			final SqlSelectBuilder builder = sql.select("ename").from("emp").where("empno").eq().value(1);
+			final SingleRowExtractor<String> extractor = rs -> rs.getString("ename");
+			builder.toQuery(extractor).toList(); // returns ["KING"]
 		}
 
-		// SELECT DISTINCT
-		{
-			final SqlSelectBuilder builder = sql.selectDistinct("name");
-			builder.from("user");
-			System.out.println(builder.getSql());
+		// DELETE examples
+		{ 
+			sql.deleteFrom("emp")
+			.where("job").notEq().value("SALESMAN");
+		}	
+
+		// UPDATE examples
+		{ 
+			sql.update("emp").set("salary").raw("salary * 2");
 		}
 
-		// Expression in SELECT clause
-		{
-			final SqlSelectBuilder builder = sql.select("'Mr' || name");
-			builder.from("user");
-			System.out.println(builder.getSql());
+		// INSERT examples
+		// Simple INSERT
+		{ 
+			final SqlInsertBuilder builder = sql.insertInto("emp");
+			builder.set("ename").value("KING");
+			builder.set("job").value("PRESIDENT");
+			builder.set("hiredate").value(new java.sql.Date(new Date().getTime()));
 		}
 
-		// Where clauses are joined with AND by default
-		{
-			final SqlSelectBuilder builder = sql.select("name");
-			builder.from("user");
-			builder.where("age").gte().value(18);
-			builder.where("age").lte().value(25);
-			System.out.println(builder.getSql());
+		// INSERT with subquery
+		{ 
+			final SqlInsertBuilder builder = sql.insertInto("emp2");
+			builder.subquery("ename", "job").select("ename", "job").from("emp");
 		}
 
-		// SELECT with OR clause
-		{
-			final SqlSelectBuilder builder = sql.select("name");
-			builder.from("user");
-			builder.where("visible").eq().value(1);
-			builder.where(sql.or(sql.condition("age").eq().value(18), sql.condition("age").eq().value(25)));
-			System.out.println(builder.getSql());
+		// SELECT examples
+		// SELECT with aggregate function
+		{ 
+			sql.select("job", "count(*)")
+			.from("emp")
+			.groupBy("job");
 		}
 
-		// Sub query in WHERE clause
-		{
-			final SqlSelectBuilder builder = sql.select("name");
-			builder.from("user");
-			builder.where("id").in(sql.select("id").from("visible_user"));
-			System.out.println(builder.getSql());
+		// Compound statement: UNION
+		{ 
+			sql.union(
+			    sql.select("job").from("emp"),
+			    sql.select("job").from("emp2")
+			);
 		}
 
-		// Simple UPDATE
-		{
-			final SqlUpdateBuilder builder = sql.update("user");
-			builder.set("name").value("John");
-			builder.where("id").eq().value(14);
-			System.out.println(builder.getSql());
-			// builder.toStatement().executeAndCommit();
+		// SELECT with AND/OR operators
+		{ 
+			final SqlSelectBuilder builder = sql.select("*");
+			builder.from("emp");
+			builder.where(sql.or(
+			    sql.condition("job").eq().value("SALESMAN"),
+			    sql.condition("name").eq().value("KING")
+			));
+			// Multiple WHERE clauses are joined with AND
+			builder.where("hiredate").lt().raw("SYSDATE - 1");
 		}
 
+		// SELECT with JOIN clause
+		{ 
+			sql.select("e.ename", "d.deptname")
+			.from("emp e")
+			.innerJoin("dept d on e.deptno = d.deptno");
+		}
+		
+
+		// Select with subquery
+		{ 
+			sql.select("*")
+			.from("emp")
+			.where("deptno").eq().subquery(
+			    sql.select("deptno")
+			    .from("dept")
+			    .where("deptname").eq().value("SALES")
+			);
+		}
+
+		// Select with IN clause
+		{ 
+			final Collection<String> names = Arrays.asList("KING", "ALLEN");
+			sql.select("*")
+			.from("emp")
+			.where("ename").in_String(names);
+		}
+		// @formatter:on
+
+		// Batch statement examples
+		// Batch statement with input data coming from a Collection
+		{
+			final List<String> values = Arrays.asList("SALES", "ACCOUNTING");
+			final List<SqlInsertBuilder> inserts = values.stream().map(ReadmeExamples::createInsertStatement)
+					.collect(Collectors.toList());
+			sql.batchStatement(inserts).executeAndCommit();
+		}
+
+		// Batch statement with input data coming from a Stream
+		{
+			final Stream<SqlInsertBuilder> stream = Files.lines(Paths.get("c:/my/file.txt"))
+					.map(ReadmeExamples::createInsertStatement);
+			// the empty string is used to generate the SQL, but anything else would
+			// have been fine since we are dealing with a prepared statement.
+			final String sqlString = createInsertStatement("").getSql();
+			sql.batchStatement(sqlString, stream).toStatement().executeAndCommit();
+		}
+	}
+
+	private static SqlInsertBuilder createInsertStatement(String deptname) {
+		final SqlInsertBuilder builder = sql.insertInto("dept");
+		builder.values().set("deptname").value(deptname);
+		return builder;
 	}
 }
