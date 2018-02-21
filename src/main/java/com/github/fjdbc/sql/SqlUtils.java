aded614fb14b2df6faa56b2816e34cb80c6e3d1b
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2018-02-21 15:39:47 +0100

    add javadoc

--- src/main/java/com/github/fjdbc/sql/SqlUtils.java
+++ src/main/java/com/github/fjdbc/sql/SqlUtils.java
@@ -12,7 +12,7 @@ public class SqlUtils {
 	public static <T> List<List<T>> partition(Collection<T> collection, final int partitionSize) {
 		assert partitionSize > 0;
 		assert collection != null;
-		return partitionList(new ArrayList<T>(collection), partitionSize);
+		return partitionList(new ArrayList<>(collection), partitionSize);
 	}
 
 	/**
@@ -22,21 +22,29 @@ public class SqlUtils {
 	private static <T> List<List<T>> partitionList(List<T> list, final int partitionSize) {
 		assert partitionSize > 0;
 		assert list != null;
-		final List<List<T>> res = new ArrayList<List<T>>();
+		final List<List<T>> res = new ArrayList<>();
 		for (int i = 0; i < list.size(); i += partitionSize) {
 			res.add(list.subList(i, Math.min(list.size(), i + partitionSize)));
 		}
 		return res;
 	}
 
+	/**
+	 * Escape a SQL comment
+	 */
 	public static String escapeComment(String comment) {
+		// @formatter:off
 		final String res = comment
 				.replace("/*", "\\slash \\star")
 				.replace("*/", "\\star \\slash")
 				.replace("--", "\\minus \\minus");
+		// @formatter:on
 		return res;
 	}
 
+	/**
+	 * Escape a SQL string literal
+	 */
 	public static String escapeString(String text) {
 		return text.replace("'", "''");
 	}
@@ -50,6 +58,9 @@ public class SqlUtils {
 		return res;
 	}
 
+	/**
+	 * Convert a Java string to a SQL literal.
+	 */
 	public static String toLiteralString(String s) {
 		return "'" + escapeString(s) + "'";
 	}
